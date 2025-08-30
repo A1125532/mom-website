841c1329ec5b3a00250b5bb3ca5ea11d8851c8aa
A1125532 <ting940511@gmail.com>
2025-08-30 21:08:12 +0800

    Update news-data.js

--- shared/news-data.js
+++ shared/news-data.js
@@ -21,7 +21,7 @@ window.NEWS_DATA = [
     title: "楊柳颱風來襲 中市議員籲零死角防汛",
     desc: "點一下看更多",
     date: "2025-08-13",
-    source: "諸葛志一",
+    source: "民眾日報／諸葛志一",
     categories: ["公共安全"],
     href: "https://tw.news.yahoo.com/%E6%A5%8A%E6%9F%B3%E9%A2%B1%E9%A2%A8%E4%BE%86%E8%A5%B2-%E4%B8%AD%E5%B8%82%E8%AD%B0%E5%93%A1%E7%B1%B2%E9%9B%B6%E6%AD%BB%E8%A7%92%E9%98%B2%E6%B1%9B-063148946.html"
   },
@@ -37,7 +37,7 @@ window.NEWS_DATA = [
     title: "豪雨釀災！陳雅惠、江肇國挺進台南 協助居民清理家園",
     desc: "點一下看更多",
     date: "2025-08-04",
-    source: "民視新聞網",
+    source: "民視新聞網／顏一軒報導",
     categories: ["公共安全"],
     href: "https://tw.news.yahoo.com/%E8%B1%AA%E9%9B%A8%E9%87%80%E7%81%BD-%E9%99%B3%E9%9B%85%E6%83%A0-%E6%B1%9F%E8%82%87%E5%9C%8B%E6%8C%BA%E9%80%B2%E5%8F%B0%E5%8D%97-%E5%8D%94%E5%8A%A9%E5%B1%85%E6%B0%91%E6%B8%85%E7%90%86%E5%AE%B6%E5%9C%92-112248190.html?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAAFot3mrRDm3WvOGzVKPNIKI6rGuMzjmYTFzySMFV5qcJ8JuWPAKOdbrKb9S9wNxHJu8HwXJWQGMwLGmlECfiQtypBzGVzzHbp0WqNi80rHCb-pon3QcM9OrdSPZ5TMuR6lJFa97M9TR5LgAMX5aBByXzAlA3yNuX8-S1Zw3w3JwH"
   },
@@ -45,7 +45,7 @@ window.NEWS_DATA = [
     title: "大罷免大決戰！請不適任藍委離開國會　陳雅惠、江肇國拚了",
     desc: "點一下看更多",
     date: "2025-07-20",
-    source: "民視新聞網",
+    source: "民視新聞網／顏一軒報導",
     categories: ["政治參與"],
     href: "https://www.ftvnews.com.tw/news/detail/2025720W0183"
   },
@@ -53,7 +53,7 @@ window.NEWS_DATA = [
     title: "台中市區下雪了? 木棉花棉絮四處飄",
     desc: "點一下看更多",
     date: "2025-06-12",
-    source: "記者蘇金鳳／台中報導",
+    source: "自由時報／記者蘇金鳳／台中報導",
     categories: ["民生"],
     href: "https://video.ltn.com.tw/article/4ymRAfZmlQo/PLI7xntdRxhw1UWsgraa6LMK0szgn9mLbn"
   },
@@ -61,7 +61,7 @@ window.NEWS_DATA = [
     title: "盧秀燕未來動向　綠議員陳雅惠議事堂直球追問",
     desc: "點一下看更多",
     date: "2025-05-28",
-    source: "記者李梅金／台中報導",
+    source: "台灣好新聞／記者李梅金／台中報導",
     categories: ["議會 / 政策"],
     href: "https://www.taiwanhot.net/news/focus/1103873/%E7%9B%A7%E7%A7%80%E7%87%95%E6%9C%AA%E4%BE%86%E5%8B%95%E5%90%91+%E7%B6%A0%E8%AD%B0%E5%93%A1%E9%99%B3%E9%9B%85%E6%83%A0%E8%AD%B0%E4%BA%8B%E5%A0%82%E7%9B%B4%E7%90%83%E8%BF%BD%E5%95%8F/80/%E6%94%BF%E6%B2%BB%E9%A0%BB%E9%81%93"
   },
@@ -77,7 +77,7 @@ window.NEWS_DATA = [
     title: "中市「文小38」荒廢多年 議員怒批已成治安死角",
     desc: "點一下看更多",
     date: "2025-05-25",
-    source: "記者蘇金鳳／台中報導",
+    source: "自由時報／記者蘇金鳳／台中報導",
     categories: ["公共安全"],
     href: "https://news.ltn.com.tw/news/life/breakingnews/5053939"
   },
@@ -85,7 +85,7 @@ window.NEWS_DATA = [
     title: "台中職災死亡5年增25人　議員籲：導入AI監控強化預警能力",
     desc: "點一下看更多",
     date: "2025-05-23",
-    source: "記者鄧木卿／台中報導",
+    source: "新聞雲／記者鄧木卿／台中報導",
     categories: ["民生"],
     href: "https://www.ettoday.net/news/20250523/2965737.htm"
   },
@@ -93,7 +93,7 @@ window.NEWS_DATA = [
     title: "垃圾車執勤頻挨罰 陳雅惠、蕭隆澤提案要求中市府速訂特種車通行規範",
     desc: "點一下看更多",
     date: "2025-05-16",
-    source: "民眾網／諸葛志一／臺中報導",
+    source: "民眾日報／諸葛志一／臺中報導",
     categories: ["議會 / 政策","民生"],
     href: "https://www.mypeoplevol.com/2025/yahoo/25222"
   },
@@ -133,8 +133,112 @@ window.NEWS_DATA = [
     title: "康橋車禍敲警鐘　陳雅惠籲盧市府：強化運動活動風險管理",
     desc: "點一下看更多",
     date: "2025-04-30",
-    source: "民時新聞網／顏一軒報導",
+    source: "民視新聞網／顏一軒報導",
     categories: ["交通"],
     href: "https://www.ftvnews.com.tw/news/detail/2025430W0554"
+  },
+  {
+    title: "台中1幼兒園驚爆虐童案 議員陳雅惠批「處理慢半拍」籲勿再容忍",
+    desc: "點一下看更多",
+    date: "2025-04-14",
+    source: "民視新聞網／林耿郁報導",
+    categories: ["教育"],
+    href: "https://reurl.cc/gYN8AQ"
+  },
+  {
+    title: "中市「好孕專車」7月上路 陳雅惠建議加速系統整合、強化資訊公開",
+    desc: "點一下看更多",
+    date: "2025-04-09",
+    source: "民眾日報／諸葛志一報導",
+    categories: ["議會 / 政策","民生"],
+    href: "https://www.mypeoplevol.com/2025/yahoo/16681"
+  },
+  {
+    title: "「痛車」也來了！台中元宵活動冒雨登場　何欣純：像搭時光機重返童年",
+    desc: "點一下看更多",
+    date: "2025-02-12",
+    source: "民視新聞網／顏一軒報導",
+    categories: ["在地活動"],
+    href: "https://www.ftvnews.com.tw/news/detail/2025212W0528"
+  },
+  {
+    title: "議員爆多局處被檢舉職場霸凌　盧秀燕：市府有多元申訴機制",
+    desc: "點一下看更多",
+    date: "2024-11-29",
+    source: "台灣好新聞／李梅金報導",
+    categories: ["議會 / 政策","民生"],
+    href: "https://reurl.cc/89Yymy"
+  },
+  {
+    title: "台中屢淹水…議員批局處別只當「沙包局」 市府說話了",
+    desc: "點一下看更多",
+    date: "2024-11-12",
+    source: "聯合報／記者趙容萱報導",
+    categories: ["民生","公共安全"],
+    href: "https://udn.com/news/story/7325/8353877"
+  },
+  {
+    title: "中市城南之心雜物佔人行道 議員批做半套、都發局長允現勘處理",
+    desc: "點一下看更多",
+    date: "2024-11-12",
+    source: "自由時報／蘇金鳳報導",
+    categories: ["在地建設"],
+    href: "https://news.ltn.com.tw/news/Taichung/breakingnews/4860029"
+  },
+  {
+    title: "里鄰整併6年掛零　綠議員批盧市府行政怠惰",
+    desc: "點一下看更多",
+    date: "2024-10-21",
+    source: "台灣好新聞／李梅金報導",
+    categories: ["議會 / 政策"],
+    href: "https://reurl.cc/MzWZlk"
+  },
+  {
+    title: "兒虐通報紀錄頻繁 陳雅惠：多關心高風險家庭狀況",
+    desc: "點一下看更多",
+    date: "2024-04-12",
+    source: "臺灣導報／張原銘、張皓傑報導",
+    categories: ["教育"],
+    href: "https://taiwanreports.com/archives/819555"
+  },
+  {
+    title: "路樹根竄進民眾住家 中市府局部斷根處理",
+    desc: "點一下看更多",
+    date: "2023-11-10",
+    source: "中央社／趙麗妍報導",
+    categories: ["民生"],
+    href: "https://www.cna.com.tw/news/aloc/202311100275.aspx"
+  },
+  {
+    title: "暑假溺水頻傳中市每年逾44人溺死　民代籲盤點風險較高水域稽查",
+    desc: "點一下看更多",
+    date: "2023-07-24",
+    source: "台灣好新聞／李梅金報導",
+    categories: ["議會 / 政策,民生"],
+    href: "https://reurl.cc/A3Xg5e"
+  },
+  {
+    title: "輕生人數居高 中市議員籲市府成立跨局處防治中心",
+    desc: "點一下看更多",
+    date: "2023-07-14",
+    source: "自由時報／蘇金鳳報導",
+    categories: ["議會 / 政策,民生"],
+    href: "https://health.ltn.com.tw/article/breakingnews/4363746"
+  },
+  {
+    title: "「往外開」防盜門不拆要挨罰？　中市都發局：影響避難才須拆除",
+    desc: "點一下看更多",
+    date: "2023-07-06",
+    source: "台灣好新聞／李梅金報導",
+    categories: ["公共安全"],
+    href: "https://news.owlting.com/articles/401125"
+  },
+  {
+    title: "台中市敬老卡使用率僅7成 市議員點出問題 盧秀燕承諾改進",
+    desc: "點一下看更多",
+    date: "2023-04-20",
+    source: "信傳媒／林伶潔報導",
+    categories: ["議會 / 政策,民生"],
+    href: "https://www.cmmedia.com.tw/home/articles/39676"
   }
 ];
