a681482d7e87d8716b3c02936429c7860ec8d6a3
A1125532 <ting940511@gmail.com>
2025-08-31 03:04:54 +0800

    Update news-data.js

--- shared/news-data.js
+++ shared/news-data.js
@@ -13,7 +13,7 @@ window.NEWS_DATA = [
     title: "綠議員指台中超徵75億元應「還稅於民」　中市府駁：近6年短絀9億元",
     desc: "點一下看更多",
     date: "2025-08-19",
-    source: "上報快訊／鍾知諭",
+    source: "上報快訊／記者鍾知諭",
     categories: ["議會 / 政策","民生"],
     href: "https://www.upmedia.mg/news_info.php?Type=24&SerialNo=237839"
   },
@@ -21,7 +21,7 @@ window.NEWS_DATA = [
     title: "楊柳颱風來襲 中市議員籲零死角防汛",
     desc: "點一下看更多",
     date: "2025-08-13",
-    source: "民眾日報／諸葛志一",
+    source: "民眾日報／記者諸葛志一",
     categories: ["公共安全"],
     href: "https://tw.news.yahoo.com/%E6%A5%8A%E6%9F%B3%E9%A2%B1%E9%A2%A8%E4%BE%86%E8%A5%B2-%E4%B8%AD%E5%B8%82%E8%AD%B0%E5%93%A1%E7%B1%B2%E9%9B%B6%E6%AD%BB%E8%A7%92%E9%98%B2%E6%B1%9B-063148946.html"
   },
@@ -29,7 +29,7 @@ window.NEWS_DATA = [
     title: "帶狀皰疹後遺症強 民眾盼中市府跟進疫苗補助",
     desc: "點一下看更多",
     date: "2025-08-11",
-    source: "大紀元／記者黃玉燕／台灣台中報導",
+    source: "大紀元／記者黃玉燕",
     categories: ["議會 / 政策","民生"],
     href: "https://www.epochtimes.com/b5/25/8/11/n14571313.htm"
   },
@@ -37,7 +37,7 @@ window.NEWS_DATA = [
     title: "豪雨釀災！陳雅惠、江肇國挺進台南 協助居民清理家園",
     desc: "點一下看更多",
     date: "2025-08-04",
-    source: "民視新聞網／顏一軒報導",
+    source: "民視新聞網／記者顏一軒",
     categories: ["公共安全"],
     href: "https://tw.news.yahoo.com/%E8%B1%AA%E9%9B%A8%E9%87%80%E7%81%BD-%E9%99%B3%E9%9B%85%E6%83%A0-%E6%B1%9F%E8%82%87%E5%9C%8B%E6%8C%BA%E9%80%B2%E5%8F%B0%E5%8D%97-%E5%8D%94%E5%8A%A9%E5%B1%85%E6%B0%91%E6%B8%85%E7%90%86%E5%AE%B6%E5%9C%92-112248190.html?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAAFot3mrRDm3WvOGzVKPNIKI6rGuMzjmYTFzySMFV5qcJ8JuWPAKOdbrKb9S9wNxHJu8HwXJWQGMwLGmlECfiQtypBzGVzzHbp0WqNi80rHCb-pon3QcM9OrdSPZ5TMuR6lJFa97M9TR5LgAMX5aBByXzAlA3yNuX8-S1Zw3w3JwH"
   },
@@ -45,7 +45,7 @@ window.NEWS_DATA = [
     title: "大罷免大決戰！請不適任藍委離開國會　陳雅惠、江肇國拚了",
     desc: "點一下看更多",
     date: "2025-07-20",
-    source: "民視新聞網／顏一軒報導",
+    source: "民視新聞網／記者顏一軒",
     categories: ["政治參與"],
     href: "https://www.ftvnews.com.tw/news/detail/2025720W0183"
   },
@@ -53,7 +53,7 @@ window.NEWS_DATA = [
     title: "台中市區下雪了? 木棉花棉絮四處飄",
     desc: "點一下看更多",
     date: "2025-06-12",
-    source: "自由時報／記者蘇金鳳／台中報導",
+    source: "自由時報／記者蘇金鳳",
     categories: ["民生"],
     href: "https://video.ltn.com.tw/article/4ymRAfZmlQo/PLI7xntdRxhw1UWsgraa6LMK0szgn9mLbn"
   },
@@ -61,7 +61,7 @@ window.NEWS_DATA = [
     title: "盧秀燕未來動向　綠議員陳雅惠議事堂直球追問",
     desc: "點一下看更多",
     date: "2025-05-28",
-    source: "台灣好新聞／記者李梅金／台中報導",
+    source: "台灣好新聞／記者李梅金",
     categories: ["議會 / 政策"],
     href: "https://www.taiwanhot.net/news/focus/1103873/%E7%9B%A7%E7%A7%80%E7%87%95%E6%9C%AA%E4%BE%86%E5%8B%95%E5%90%91+%E7%B6%A0%E8%AD%B0%E5%93%A1%E9%99%B3%E9%9B%85%E6%83%A0%E8%AD%B0%E4%BA%8B%E5%A0%82%E7%9B%B4%E7%90%83%E8%BF%BD%E5%95%8F/80/%E6%94%BF%E6%B2%BB%E9%A0%BB%E9%81%93"
   },
@@ -69,7 +69,7 @@ window.NEWS_DATA = [
     title: "台中里長邊騎機車邊對議員比中指 公然侮辱判拘役20日",
     desc: "點一下看更多",
     date: "2025-05-28",
-    source: "聯合報／記者曾健祐／台中即時報導",
+    source: "聯合報／記者曾健祐",
     categories: ["政治參與"],
     href: "https://udn.com/news/story/7321/8769683"
   },
@@ -77,7 +77,7 @@ window.NEWS_DATA = [
     title: "中市「文小38」荒廢多年 議員怒批已成治安死角",
     desc: "點一下看更多",
     date: "2025-05-25",
-    source: "自由時報／記者蘇金鳳／台中報導",
+    source: "自由時報／記者蘇金鳳",
     categories: ["公共安全"],
     href: "https://news.ltn.com.tw/news/life/breakingnews/5053939"
   },
@@ -85,7 +85,7 @@ window.NEWS_DATA = [
     title: "台中職災死亡5年增25人　議員籲：導入AI監控強化預警能力",
     desc: "點一下看更多",
     date: "2025-05-23",
-    source: "新聞雲／記者鄧木卿／台中報導",
+    source: "新聞雲／記者鄧木卿",
     categories: ["民生"],
     href: "https://www.ettoday.net/news/20250523/2965737.htm"
   },
@@ -93,7 +93,7 @@ window.NEWS_DATA = [
     title: "垃圾車執勤頻挨罰 陳雅惠、蕭隆澤提案要求中市府速訂特種車通行規範",
     desc: "點一下看更多",
     date: "2025-05-16",
-    source: "民眾日報／諸葛志一／臺中報導",
+    source: "民眾日報／記者諸葛志一",
     categories: ["議會 / 政策","民生"],
     href: "https://www.mypeoplevol.com/2025/yahoo/25222"
   },
@@ -101,7 +101,7 @@ window.NEWS_DATA = [
     title: "中捷藍線今完成簽約　陳雅惠籲盧市府：補足東南區路網空缺",
     desc: "點一下看更多",
     date: "2025-05-14",
-    source: "民視新聞網／顏一軒報導",
+    source: "民視新聞網／記者顏一軒",
     categories: ["民生","交通"],
     href: "https://www.ftvnews.com.tw/news/detail/2025514W0596"
   },
@@ -109,7 +109,7 @@ window.NEWS_DATA = [
     title: "張芬郁、陳雅惠盼推高齡公寓　李正偉：研議公辦民營老人寓或青銀混居",
     desc: "點一下看更多",
     date: "2025-05-12",
-    source: "CNEWS匯流新聞網／記者徐德芬／台中報導",
+    source: "CNEWS匯流新聞網／記者徐德芬",
     categories: ["民生"],
     href: "https://cnews.com.tw/237250512a05/"
   },
@@ -117,7 +117,7 @@ window.NEWS_DATA = [
     title: "都更牛步！台中平均屋齡逼近30年 外來遊客驚「不像先進城市」",
     desc: "點一下看更多",
     date: "2025-05-12",
-    source: "自由時報／記者蘇孟娟／台中報導",
+    source: "自由時報／記者蘇孟娟",
     categories: ["在地建設"],
     href: "https://news.ltn.com.tw/news/Taichung/breakingnews/5040101"
   },
@@ -125,23 +125,55 @@ window.NEWS_DATA = [
     title: "機車「綠燈左轉」遭開罰！ 中市議員陳雅惠：標準混亂、執法過當",
     desc: "點一下看更多",
     date: "2025-05-07",
-    source: "民視新聞網／記者林耿郁／台中報導",
+    source: "民視新聞網／記者林耿郁",
     categories: ["交通"],
     href: "https://reurl.cc/OmQX8D"
+  },
+  {
+    title: "路牌模糊、標線掉漆、路燈不亮　陳雅惠等六位議員聯合質詢交通局長葉昭甫：市政基本功不能爛！",
+    desc: "點一下看更多",
+    date: "2025-05-02",
+    source: "新華報導",
+    categories: ["議會 / 政策,交通"],
+    href: "https://ccss3172.blogspot.com/2025/05/blog-post_27.html"
+  },
+  {
+    title: "母親節親子同樂 東區大智公園眼科免費義診",
+    desc: "點一下看更多",
+    date: "2025-05-01",
+    source: "大紀元／記者黃玉燕",
+    categories: ["在地活動"],
+    href: "https://www.epochtimes.com/b5/25/5/1/n14496166.htm"
   },
   {
     title: "康橋車禍敲警鐘　陳雅惠籲盧市府：強化運動活動風險管理",
     desc: "點一下看更多",
     date: "2025-04-30",
-    source: "民視新聞網／顏一軒報導",
+    source: "民視新聞網／記者顏一軒",
     categories: ["交通"],
     href: "https://www.ftvnews.com.tw/news/detail/2025430W0554"
+  },
+  {
+    title: "台中建置公安稽查管理平台 陳雅惠籲強化預警與資安",
+    desc: "點一下看更多",
+    date: "2025-04-25",
+    source: "聯合報／記者陳秋雲",
+    categories: ["議會 / 政策,公共安全"],
+    href: "https://udn.com/news/story/7323/8698506"
+  },
+  {
+    title: "避免再發生氣爆意外 中市議員陳雅惠要求建置高風險業者專案列管制度",
+    desc: "點一下看更多",
+    date: "2025-04-24",
+    source: "中華新聞網／記者楊文琳",
+    categories: ["議會 / 政策"],
+    href: "https://www.cdns.com.tw/articles/1206432"
   },
   {
     title: "台中市公托不足 民進黨台中市議員陳雅惠批：中市府拖延 家長苦等",
     desc: "點一下看更多",
     date: "2025-04-21",
-    source: "元丰傳媒／林獻元報導",
+    source: "元丰傳媒／記者林獻元",
     categories: ["教育"],
     href: "https://yuanfengmedia.tw/detail.php?type=lastest&id=40734"
   },
@@ -149,7 +181,7 @@ window.NEWS_DATA = [
     title: "台中1幼兒園驚爆虐童案 議員陳雅惠批「處理慢半拍」籲勿再容忍",
     desc: "點一下看更多",
     date: "2025-04-14",
-    source: "民視新聞網／林耿郁報導",
+    source: "民視新聞網／記者林耿郁",
     categories: ["教育"],
     href: "https://reurl.cc/gYN8AQ"
   },
@@ -157,15 +189,39 @@ window.NEWS_DATA = [
     title: "中市「好孕專車」7月上路 陳雅惠建議加速系統整合、強化資訊公開",
     desc: "點一下看更多",
     date: "2025-04-09",
-    source: "民眾日報／諸葛志一報導",
+    source: "民眾日報／記者諸葛志一",
     categories: ["議會 / 政策","民生"],
     href: "https://www.mypeoplevol.com/2025/yahoo/16681"
+  },
+  {
+    title: "台中罷免廟口開講 綠議員：「1人拉5人」罷免會成功",
+    desc: "點一下看更多",
+    date: "2025-03-30",
+    source: "自由時報／記者陳建志",
+    categories: ["政治參與"],
+    href: "https://news.ltn.com.tw/news/politics/breakingnews/4996293"
+  },
+  {
+    title: "全台大罷免進入二階連 中市議員陳雅惠騎自行車宣傳力挺民團",
+    desc: "點一下看更多",
+    date: "2025-03-13",
+    source: "自由時報／記者蘇金鳳",
+    categories: ["政治參與"],
+    href: "https://news.ltn.com.tw/news/Taichung/breakingnews/4978590"
+  },
+  {
+    title: "為民進黨首場宣講熱身！中市女議員沿街敲小鑼　喊罷羅廷瑋",
+    desc: "點一下看更多",
+    date: "2025-03-13",
+    source: "NOWnews 今日新聞／記者顏幸如",
+    categories: ["政治參與"],
+    href: "https://www.nownews.com/news/6658240"
   },
   {
     title: "半小時被搶光！台中綠議員陳雅惠代贈700株樹苗　籲共同美化環境",
     desc: "點一下看更多",
     date: "2025-03-05",
-    source: "民視新聞網／顏一軒報導",
+    source: "民視新聞網／記者顏一軒",
     categories: ["在地活動"],
     href: "https://finance.ftvnews.com.tw/news/detail/2025305W0529"
   },
@@ -173,15 +229,47 @@ window.NEWS_DATA = [
     title: "「痛車」也來了！台中元宵活動冒雨登場　何欣純：像搭時光機重返童年",
     desc: "點一下看更多",
     date: "2025-02-12",
-    source: "民視新聞網／顏一軒報導",
+    source: "民視新聞網／記者顏一軒",
     categories: ["在地活動"],
     href: "https://www.ftvnews.com.tw/news/detail/2025212W0528"
+  },
+  {
+    title: "英系議員合體掛「恭賀新春」看板 選民：支持何欣純更上層樓",
+    desc: "點一下看更多",
+    date: "2025-01-25",
+    source: "Newtalk新聞／記者唐復年",
+    categories: ["政治參與"],
+    href: "https://newtalk.tw/news/view/2025-01-25/954562"
+  },
+  {
+    title: "遲未領到購物節獎金 台中市府挨批效率差",
+    desc: "點一下看更多",
+    date: "2025-01-24",
+    source: "自由時報／記者蘇金鳳",
+    categories: ["民生"],
+    href: "https://news.ltn.com.tw/news/life/paper/1688921"
+  },
+  {
+    title: "國光里福正宮迎新年 尾牙分享湯圓、贈春聯",
+    desc: "點一下看更多",
+    date: "2025-01-15",
+    source: "大紀元／記者黃玉燕",
+    categories: ["在地活動"],
+    href: "https://www.epochtimes.com/b5/25/1/15/n14413988.htm"
+  },
+  {
+    title: "「蛇麼都要」 台中東興里客製化春聯供索取",
+    desc: "點一下看更多",
+    date: "2025-01-11",
+    source: "大紀元／記者黃玉燕",
+    categories: ["在地活動"],
+    href: "https://www.epochtimes.com/b5/25/1/11/n14411236.htm"
   },
   {
     title: "中市水噹噹關懷協會寒冬送暖 白米物資慰問金助弱勢過好年",
     desc: "點一下看更多",
     date: "2025-01-08",
-    source: "中廣新聞網／寇世菁報導",
+    source: "中廣新聞網／記者寇世菁",
     categories: ["在地活動"],
     href: "https://reurl.cc/2Q7ry9"
   },
@@ -189,7 +277,7 @@ window.NEWS_DATA = [
     title: "議員爆多局處被檢舉職場霸凌　盧秀燕：市府有多元申訴機制",
     desc: "點一下看更多",
     date: "2024-11-29",
-    source: "台灣好新聞／李梅金報導",
+    source: "台灣好新聞／記者李梅金",
     categories: ["議會 / 政策","民生"],
     href: "https://reurl.cc/89Yymy"
   },
@@ -197,7 +285,7 @@ window.NEWS_DATA = [
     title: "台中屢淹水…議員批局處別只當「沙包局」 市府說話了",
     desc: "點一下看更多",
     date: "2024-11-12",
-    source: "聯合報／記者趙容萱報導",
+    source: "聯合報／記者趙容萱",
     categories: ["民生","公共安全"],
     href: "https://udn.com/news/story/7325/8353877"
   },
@@ -205,31 +293,87 @@ window.NEWS_DATA = [
     title: "中市城南之心雜物佔人行道 議員批做半套、都發局長允現勘處理",
     desc: "點一下看更多",
     date: "2024-11-12",
-    source: "自由時報／蘇金鳳報導",
+    source: "自由時報／記者蘇金鳳",
     categories: ["在地建設"],
     href: "https://news.ltn.com.tw/news/Taichung/breakingnews/4860029"
+  },
+  {
+    title: "台中BMW猴無照還疑毒駕撞死人 「與議員是親戚」媽衝分隊長室談遭質疑",
+    desc: "點一下看更多",
+    date: "2024-10-28",
+    source: "鏡週刊／記者謝文哲",
+    categories: ["交通,其他"],
+    href: "https://tw.news.yahoo.com/%E5%8F%B0%E4%B8%ADbmw%E7%8C%B4%E7%84%A1%E7%85%A7%E9%82%84%E7%96%91%E6%AF%92%E9%A7%95%E6%92%9E%E6%AD%BB%E4%BA%BA-%E8%88%87%E8%AD%B0%E5%93%A1%E6%98%AF%E8%A6%AA%E6%88%9A-%E5%AA%BD%E8%A1%9D%E5%88%86%E9%9A%8A%E9%95%B7%E5%AE%A4%E8%AB%87%E9%81%AD%E8%B3%AA%E7%96%91-060500062.html"
+  },
+  {
+    title: "盧秀燕親自擔任台中購物節代言人 議員酸市府砸錢造神",
+    desc: "點一下看更多",
+    date: "2024-10-24",
+    source: "臺灣時報／記者鍾麗如",
+    categories: ["議會 / 政策"],
+    href: "https://www.taiwantimes.com.tw/app-container/app-content/new/new-content-detail?blogId=blog-8ba4ea64-b5f5-4401-acdf-ecb8eee2d3ce&currentCategory=11"
   },
   {
     title: "里鄰整併6年掛零　綠議員批盧市府行政怠惰",
     desc: "點一下看更多",
     date: "2024-10-21",
-    source: "台灣好新聞／李梅金報導",
+    source: "台灣好新聞／記者李梅金",
     categories: ["議會 / 政策"],
     href: "https://reurl.cc/MzWZlk"
+  },
+  {
+    title: "崇德殯儀館改建議員憂黑暗期 中市府：計劃增600多車位",
+    desc: "點一下看更多",
+    date: "2024-10-08",
+    source: "聯合報／記者陳秋雲",
+    categories: ["議會 / 政策"],
+    href: "https://udn.com/news/story/7325/8277587"
+  },
+  {
+    title: "中市近七成屋齡逾25年 議員促危老重建",
+    desc: "點一下看更多",
+    date: "2024-05-16",
+    source: "大紀元／記者黃玉燕",
+    categories: ["議會 / 政策","民生"],
+    href: "https://www.epochtimes.com/b5/24/5/16/n14251640.htm"
   },
   {
     title: "民進黨台中市議員陳雅惠等要求衛生局提升長照服務的品質 減輕家庭照顧者壓力",
     desc: "點一下看更多",
     date: "2024-05-13",
-    source: "元丰傳媒／林獻元報導",
+    source: "元丰傳媒／記者林獻元",
     categories: ["議會 / 政策","民生"],
     href: "https://www.yuanfengmedia.tw/detail.php?type=gallery&id=29138"
+  },
+  {
+    title: "中捷藍線未見動工 陳雅惠等議員要求盧秀燕兌現承諾",
+    desc: "點一下看更多",
+    date: "2024-05-08",
+    source: "自由時報／記者蘇金鳳",
+    categories: ["議會 / 政策","交通"],
+    href: "https://news.ltn.com.tw/news/politics/breakingnews/4666773"
+  },
+  {
+    title: "騎樓無法停汽車屢遭開罰　中市議員籲制定自治條例解套",
+    desc: "點一下看更多",
+    date: "2024-05-06",
+    source: "台灣好新聞／記者李梅金",
+    categories: ["議會 / 政策"],
+    href: "https://www.taiwanhot.net/news/1067441/%E9%A8%8E%E6%A8%93%E7%84%A1%E6%B3%95%E5%81%9C%E6%B1%BD%E8%BB%8A%E5%B1%A2%E9%81%AD%E9%96%8B%E7%BD%B0+%E4%B8%AD%E5%B8%82%E8%AD%B0%E5%93%A1%E7%B1%B2%E5%88%B6%E5%AE%9A%E8%87%AA%E6%B2%BB%E6%A2%9D%E4%BE%8B%E8%A7%A3%E5%A5%97"
+  },
+  {
+    title: "購物節走國際化鎖定日本　綠營中市議員質疑光靠抽獎能吸客？",
+    desc: "點一下看更多",
+    date: "2024-04-26",
+    source: "台灣好新聞／記者李梅金",
+    categories: ["議會 / 政策","民生"],
+    href: "https://today.line.me/tw/v3/article/zNBREJg"
   },
   {
     title: "兒虐通報紀錄頻繁 陳雅惠：多關心高風險家庭狀況",
     desc: "點一下看更多",
     date: "2024-04-12",
-    source: "臺灣導報／張原銘、張皓傑報導",
+    source: "臺灣導報／記者張原銘、張皓傑",
     categories: ["教育"],
     href: "https://taiwanreports.com/archives/819555"
   },
@@ -237,7 +381,7 @@ window.NEWS_DATA = [
     title: "路樹根竄進民眾住家 中市府局部斷根處理",
     desc: "點一下看更多",
     date: "2023-11-10",
-    source: "中央社／趙麗妍報導",
+    source: "中央社／記者趙麗妍",
     categories: ["民生"],
     href: "https://www.cna.com.tw/news/aloc/202311100275.aspx"
   },
@@ -245,7 +389,7 @@ window.NEWS_DATA = [
     title: "暑假溺水頻傳中市每年逾44人溺死　民代籲盤點風險較高水域稽查",
     desc: "點一下看更多",
     date: "2023-07-24",
-    source: "台灣好新聞／李梅金報導",
+    source: "台灣好新聞／記者李梅金",
     categories: ["議會 / 政策,民生"],
     href: "https://reurl.cc/A3Xg5e"
   },
@@ -253,7 +397,7 @@ window.NEWS_DATA = [
     title: "輕生人數居高 中市議員籲市府成立跨局處防治中心",
     desc: "點一下看更多",
     date: "2023-07-14",
-    source: "自由時報／蘇金鳳報導",
+    source: "自由時報／記者蘇金鳳",
     categories: ["議會 / 政策,民生"],
     href: "https://health.ltn.com.tw/article/breakingnews/4363746"
   },
@@ -261,16 +405,200 @@ window.NEWS_DATA = [
     title: "「往外開」防盜門不拆要挨罰？　中市都發局：影響避難才須拆除",
     desc: "點一下看更多",
     date: "2023-07-06",
-    source: "台灣好新聞／李梅金報導",
+    source: "台灣好新聞／記者李梅金",
     categories: ["公共安全"],
     href: "https://news.owlting.com/articles/401125"
+  },
+  {
+    title: "中市議員為百年歷史半平厝福德祠請命　籲原址保留重建",
+    desc: "點一下看更多",
+    date: "2023-05-16",
+    source: "台灣好新聞／記者李梅金",
+    categories: ["在地建設"],
+    href: "https://www.taiwanhot.net/news/1032152/%E4%B8%AD%E5%B8%82%E8%AD%B0%E5%93%A1%E7%82%BA%E7%99%BE%E5%B9%B4%E6%AD%B7%E5%8F%B2%E5%8D%8A%E5%B9%B3%E5%8E%9D%E7%A6%8F%E5%BE%B7%E7%A5%A0%E8%AB%8B%E5%91%BD+%E7%B1%B2%E5%8E%9F%E5%9D%80%E4%BF%9D%E7%95%99%E9%87%8D%E5%BB%BA"
   },
   {
     title: "台中市敬老卡使用率僅7成 市議員點出問題 盧秀燕承諾改進",
     desc: "點一下看更多",
     date: "2023-04-20",
-    source: "信傳媒／林伶潔報導",
+    source: "信傳媒／記者林伶潔",
     categories: ["議會 / 政策,民生"],
     href: "https://www.cmmedia.com.tw/home/articles/39676"
+  },
+  {
+    title: "市議員陳雅惠關心鋒面來襲是否帶來災情，要求水利局掌握災情及時處理，保障民眾生命財產安全",
+    desc: "點一下看更多",
+    date: "2023-04-20",
+    source: "新華報導／記者魏永忠",
+    categories: ["議會 / 政策,公共安全"],
+    href: "https://ccss3172.blogspot.com/2023/04/blog-post_384.html"
+  },
+  {
+    title: "民進黨中市六選區戰況激烈 江肇國一早站路口還有英系陳雅惠陪同",
+    desc: "點一下看更多",
+    date: "2023-03-28",
+    source: "自由時報／記者蘇金鳳",
+    categories: ["政治參與"],
+    href: "https://news.ltn.com.tw/news/politics/breakingnews/4253904"
+  },
+  {
+    title: "盧秀燕拜訪議員談市政　陳雅惠暖心送市長「平安吉利」",
+    desc: "點一下看更多",
+    date: "2023-01-06",
+    source: "台灣好新聞／記者黃志政",
+    categories: ["其他"],
+    href: "https://n.yam.com/Article/20230106777406"
+  },
+  {
+    title: "中市意外頻傳 議員建議盧秀燕拜拜安民心",
+    desc: "點一下看更多",
+    date: "2023-01-06",
+    source: "民眾日報／記者諸葛志一",
+    categories: ["其他"],
+    href: "https://tw.news.yahoo.com/%E4%B8%AD%E5%B8%82%E6%84%8F%E5%A4%96%E9%A0%BB%E5%82%B3-%E8%AD%B0%E5%93%A1%E5%BB%BA%E8%AD%B0%E7%9B%A7%E7%A7%80%E7%87%95%E6%8B%9C%E6%8B%9C%E5%AE%89%E6%B0%91%E5%BF%83-084328250.html"
+  },
+  {
+    title: "國光里長陳雅惠接棒當選議員　「以母之名」接軌服務之路",
+    desc: "點一下看更多",
+    date: "2022-11-28",
+    source: "台灣好新聞／記者黃志政",
+    categories: ["政治參與"],
+    href: "https://reurl.cc/MzWbXn"
+  },
+  {
+    title: "東南區新人陳雅惠當選與母邱素貞感謝支持",
+    desc: "點一下看更多",
+    date: "2022-11-26",
+    source: "中時新聞網／記者張妍溱",
+    categories: ["政治參與"],
+    href: "https://www.chinatimes.com/realtimenews/20221126003322-260407?chdtv"
+  },
+  {
+    title: "選前倒數兩天　民進黨東南區市議員候選人陳雅惠車掃告急",
+    desc: "點一下看更多",
+    date: "2022-11-24",
+    source: "台灣好新聞／記者黃志政",
+    categories: ["政治參與"],
+    href: "https://reurl.cc/x3eOlE"
+  },
+  {
+    title: "前副總統陳建仁力挺　讚陳雅惠是好人才",
+    desc: "點一下看更多",
+    date: "2022-11-16",
+    source: "台灣好新聞／記者黃志政",
+    categories: ["政治參與"],
+    href: "https://reurl.cc/ko97lq"
+  },
+  {
+    title: "搶救民進黨東南區唯一女性議員　蔡其昌：陳雅惠這一席「幫我顧起來」",
+    desc: "點一下看更多",
+    date: "2022-11-11",
+    source: "台灣好新聞／記者黃志政",
+    categories: ["政治參與"],
+    href: "https://reurl.cc/ZN8rXV"
+  },
+  {
+    title: "Q版陳雅惠登場 俏皮、可愛超吸睛",
+    desc: "點一下看更多",
+    date: "2022-10-30",
+    source: "新華報導／記者魏永忠",
+    categories: ["其他"],
+    href: "https://ccss3172.blogspot.com/2022/10/q_30.html"
+  },
+  {
+    title: "中市東南區議員 11 人搶 5 席選情激烈　陳雅惠危急喊「確保民進黨唯一女性」",
+    desc: "點一下看更多",
+    date: "2022-10-24",
+    source: "台灣好新聞／記者黃志政",
+    categories: ["政治參與"],
+    href: "https://news.owlting.com/articles/195984"
+  },
+  {
+    title: "市議員候選人陳雅惠晚間在忠孝路觀光夜市 在攤販集中區管理委員會主委曾再富陪同下，到忠孝夜市掃街拜票",
+    desc: "點一下看更多",
+    date: "2022-10-14",
+    source: "新華報導／記者魏永忠",
+    categories: ["政治參與"],
+    href: "https://udn.com/news/story/7325/8277587https://ccss3172.blogspot.com/2022/10/blog-post_455.html"
+  },
+  {
+    title: "陳雅惠、黃淑紋 年底將分別參選市議員與國光里長",
+    desc: "點一下看更多",
+    date: "2022-07-06",
+    source: "新華報導／記者董維宣",
+    categories: ["政治參與"],
+    href: "https://ccss3172.blogspot.com/2022/07/blog-post_186.html"
+  },
+  {
+    title: "台中東南區初選 何昆霖、鄭功進、陳雅惠勝出",
+    desc: "點一下看更多",
+    date: "2022-05-13",
+    source: "大紀元／記者黃玉燕",
+    categories: ["政治參與"],
+    href: "https://www.epochtimes.com/b5/22/5/13/n13735490.htm"
+  },
+  {
+    title: "中市議員邱素貞女兒陳雅惠接棒參選　立委黃國書「同框」相挺",
+    desc: "點一下看更多",
+    date: "2021-10-12",
+    source: "台灣好新聞／記者黃志政",
+    categories: ["政治參與"],
+    href: "https://reurl.cc/bmek7r"
+  },
+  {
+    title: "2022選戰搶造勢 議員邱素貞宣布交棒第二代",
+    desc: "點一下看更多",
+    date: "2021-10-01",
+    source: "大紀元／記者黃玉燕",
+    categories: ["政治參與"],
+    href: "https://www.epochtimes.com/b5/21/10/1/n13274022.htm"
+  },
+  {
+    title: "萬聖節搗蛋逗陣行　陳雅惠里長揪數百學童同樂",
+    desc: "點一下看更多",
+    date: "2020-10-31",
+    source: "台灣好新聞／記者李梅金",
+    categories: ["在地活動"],
+    href: "https://today.line.me/tw/v3/article/69PD2X"
+  },
+  {
+    title: "接替父親遺志 中市國光里長陳雅惠今就職",
+    desc: "點一下看更多",
+    date: "2019-05-29",
+    source: "Newtalk新聞／記者唐復年",
+    categories: ["政治參與"],
+    href: "https://newtalk.tw/news/view/2019-05-29/253172"
+  },
+  {
+    title: "繼承父親衣缽！台中國光里長補選 前里長女兒陳雅惠當選",
+    desc: "點一下看更多",
+    date: "2019-05-11",
+    source: "自由時報／記者張瑞楨",
+    categories: ["政治參與"],
+    href: "https://news.ltn.com.tw/news/politics/breakingnews/2787214"
+  },
+  {
+    title: "陳雅惠代父出征國光里長補選　母親披彩帶場面溫馨",
+    desc: "點一下看更多",
+    date: "2019-05-05",
+    source: "台灣好新聞／記者官達里",
+    categories: ["政治參與"],
+    href: "https://reurl.cc/A3XKAp"
+  },
+  {
+    title: "台中國光里長陳仁榮過世出缺　女兒陳雅惠承繼遺志出征補選",
+    desc: "點一下看更多",
+    date: "2019-03-27",
+    source: "台灣好新聞／記者黃志政",
+    categories: ["政治參與"],
+    href: "https://reurl.cc/GNgoA3"
+  },
+  {
+    title: "中市南區國光里長病逝 女兒陳雅惠宣布出征",
+    desc: "點一下看更多",
+    date: "2019-05-26",
+    source: "自由時報／記者張瑞楨",
+    categories: ["政治參與"],
+    href: "https://news.ltn.com.tw/news/politics/breakingnews/2739285"
   }
 ];
